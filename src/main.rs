80e913739eaac26d45185707463dba77c4f85f9b
Joe Dodd <joehdodd@gmail.com>
2023-12-15 20:13:50 -0500

    Fixes get handler's idenity crisis.

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ struct WeatherData {
     current: Current,
 }
 
-async fn get_todos() -> Json<WeatherData> {
+async fn get_weather() -> Json<WeatherData> {
     let key = std::env::var("WEATHER_API_KEY").unwrap();
     let request_url = format!("http://api.weatherstack.com/current?access_key={}&query=New York", key);
     let client = reqwest::Client::new();
@@ -66,7 +66,7 @@ async fn get_todos() -> Json<WeatherData> {
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    let app = Router::new().route("/", get(get_todos));
+    let app = Router::new().route("/", get(get_weather));
     let listener = tokio::net::TcpListener::bind("localhost:1337")
         .await
         .unwrap();
